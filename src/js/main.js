250210f42f8dc67f79b332449431025152a919dd
EdwardNewsky <edwardnewsky@gmail.com>
2021-07-07 09:41:25 +0300

    шаблон сделал

--- src/js/main.js
+++ src/js/main.js
@@ -44,3 +44,20 @@ var swiper = new Swiper(".swiper__products", {
     },
   },
 });
+
+// // ---------------------
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
